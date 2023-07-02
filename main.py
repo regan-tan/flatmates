d38ae71d4fd2368359fa0a363f290aefe270f3f5
Regan Tan <regantanyk@gmailcom>
2023-07-02 15:01:45 +0800

    auto open generated pdf in web; included houseicon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fpdf import FPDF
+import webbrowser
 
 class Bill:
 
@@ -39,22 +40,38 @@ class PdfReport:
 
     def generate(self, flatmate1, flatmate2, bill):
 
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
+        #Add image to pdf
+        pdf.image("house.jpeg", w=50, h=50)
+
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=0, align="C", ln=1)
 
         # Insert Period label and value
-        pdf.cell(w=100, h=40, txt='Period', border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.set_font(family='Times', size = 16, style='B')
+        pdf.cell(w=100, h=40, txt='Period', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
 
         # Insert Name and bill amount of first flatmate
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1, ln=1)
+        pdf.set_font(family='Times', size = 12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert Name and bill amount of second flatmate
+        pdf.set_font(family='Times', size = 12)
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
 
         pdf.output(self.filename)
+
+        # Automatically open web browser to view generated pdf flatmate bill report
+        webbrowser.open(self.filename)
 
 bill = Bill(amount = 1200, period = "August 2021")
 john = Flatmate(name="John", days_in_house=20)
